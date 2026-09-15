60e8017f79b2ba36d87339b0dfb3fe7f11d7dede
Henrique Mendes <hapmendesss@gmail.com>
2026-09-15 03:47:43 -0300

    vimrc upd

--- codes/extra/vimrc/vimrc.cpp
+++ codes/extra/vimrc/vimrc.cpp
@@ -1,9 +1,4 @@
 syntax on
-set ts=4 sw=4 sta et nu rnu sc cindent autoread mouse=a is hls
-
+set ts=4 sw=4 et nu rnu ai si cin mouse=a is hls gd so=5
 inoremap {<CR> {<CR>}<Esc>O
-nnoremap <C-down> :m+1<CR>
-nnoremap <C-up> :m-2<CR>
-
-nnoremap <F5> :w<CR>:!g++ -fsanitize=address -g % -o %:r && ./%:r<CR>
-nnoremap <F6> :w<CR>:!g++ -fsanitize=address -g % -o %:r && ./%:r < <C-r>=input('file: ')<CR><CR>
+nnoremap <F2> *:%s//
